c427df8ca5d151b3dd4191d089ad8a7887ebb55e
Kimberly Reddick <kim.reddick@icloud.com>
2021-11-30 16:24:34 -0600

    Setup Database

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for app project.
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
